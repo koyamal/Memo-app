84517a121908473c86407c3a6a0540fa7035e094
koyamal <tanakamaruko1121n@gmail.com>
2023-04-17 18:45:37 +0900

    Create MemoEditScreen component

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 
 export default function App() {
   return (
-    <MemoListScreen />
+    // <MemoListScreen />
     // <MemoDetailScreen />
+    <MemoEditScreen />
   );
 }
---
+++ src/screens/MemoEditScreen.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+} from 'react-native';
+import AppBar from '../components/AppBar';
+import CircleButton from '../components/CircleButton';
+
+export default function MemoEditScreen() {
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <View style={styles.inputContainer}>
+        <TextInput value="買い物リスト" multiline style={styles.input} />
+      </View>
+      <CircleButton name="check" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    paddingVertical: 32,
+    paddingHorizontal: 27,
+    flex: 1,
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: 'top',
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
