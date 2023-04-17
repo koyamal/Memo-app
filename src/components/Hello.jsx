f75615571bea9c4aa87a7cf613e8fb9c77c512b2
koyamal <tanakamaruko1121n@gmail.com>
2023-04-17 14:40:57 +0900

    Add style to Hello component

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -4,9 +4,19 @@ import { View, Text, StyleSheet } from 'react-native';
 function Hello() {
   return (
     <View>
-      <Text>Hello</Text>
+      <Text style={styles.text}>Hello</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#ffffff',
+    backgroundColor: 'blue',
+    fontSize: 40,
+    fontWeight: 'bold',
+    padding: 16,
+  },
+});
 
 export default Hello;
