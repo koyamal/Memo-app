0cdab2f1e4b75d434c53b65d39e98aac67a85d3a
koyamal <tanakamaruko1121n@gmail.com>
2023-04-17 15:36:19 +0900

    Add another props to Hello component

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 /* eslint-disable-next-line */
-import { string } from 'prop-types';
+import { string, bool } from 'prop-types';
 
 function Hello(props) {
-  const { children } = props;
+  const { children, bang } = props;
   return (
     <View>
       <Text style={styles.text}>
-        {`Hello ${children}`}
+        {`Hello ${children}${bang ? '!' : ''}`}
       </Text>
     </View>
   );
@@ -16,6 +16,11 @@ function Hello(props) {
 
 Hello.propTypes = {
   children: string.isRequired,
+  bang: bool,
+};
+
+Hello.defaultProps = {
+  bang: false,
 };
 
 const styles = StyleSheet.create({
