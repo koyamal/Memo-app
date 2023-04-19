21898361ae6a92c60543551cc9da0430f522929a
koyamal <tanakamaruko1121n@gmail.com>
2023-04-20 08:14:45 +0900

    Add root from LogIn/SignUp to MemoList

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -8,14 +8,15 @@ import {
 } from 'react-native';
 import Button from '../components/Button';
 
-export default function SignUpScreen() {
+export default function SignUpScreen(props) {
+  const { navigation } = props;
   return (
     <View style={styles.container}>
       <View style={styles.inner}>
         <Text style={styles.title}>Sign Up</Text>
         <TextInput style={styles.input} value="Email Address" />
         <TextInput style={styles.input} value="Password" />
-        <Button label="Submit" />
+        <Button label="Submit" onPress={() => { navigation.navigate('MemoList'); }} />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Already registered?</Text>
           <TouchableOpacity>
