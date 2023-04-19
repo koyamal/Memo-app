9f3e9bee9fc640f803ba6815db24f4d0618353c4
koyamal <tanakamaruko1121n@gmail.com>
2023-04-19 12:31:27 +0900

    Use NavigationContainer/createStackNavigator

--- App.jsx
+++ App.jsx
@@ -1,16 +1,26 @@
+/* eslint import/no-extraneous-dependencies: 0 */
 import React from 'react';
-// import MemoListScreen from './src/screens/MemoListScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoCreateScreen from './src/screens/MemoCreateScreen';
 // import LogInScreen from './src/screens/LogInScreen';
 // import SignUpScreen from './src/screens/SignUpScreen';
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MemoListScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
     // <MemoListScreen />
     // <MemoDetailScreen />
-    <MemoEditScreen />
+    // <MemoEditScreen />
     // <MemoCreateScreen />
     // <LogInScreen />
     // <SignUpScreen />
