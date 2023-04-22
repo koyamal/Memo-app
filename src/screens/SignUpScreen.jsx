fbc43678e1607a3a0e7ee2b12e04085bee0eae49
koyamal <tanakamaruko1121n@gmail.com>
2023-04-22 11:41:20 +0900

    Use useState in SignUpScreen

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   StyleSheet,
@@ -9,13 +9,23 @@ import {
 import Button from '../components/Button';
 
 export default function SignUpScreen(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const { navigation } = props;
   return (
     <View style={styles.container}>
       <View style={styles.inner}>
         <Text style={styles.title}>Sign Up</Text>
-        <TextInput style={styles.input} value="Email Address" />
-        <TextInput style={styles.input} value="Password" />
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={(text) => { setEmail(text); }}
+        />
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={(text) => { setPassword(text); }}
+        />
         <Button
           label="Submit"
           onPress={() => {
