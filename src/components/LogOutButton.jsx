206b93924e25f77e9be1adf50dc442671ab08945
koyamal <tanakamaruko1121n@gmail.com>
2023-04-23 11:32:39 +0900

    Create function to LogOut

--- src/components/LogOutButton.jsx
+++ src/components/LogOutButton.jsx
@@ -1,9 +1,33 @@
+/* eslint import/no-extraneous-dependencies: 0 */
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import {
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Alert,
+} from 'react-native';
+import firebase from 'firebase';
+import { useNavigation } from '@react-navigation/native';
 
 export default function LogOutButton() {
+  const navigation = useNavigation();
+  function handlePress() {
+    firebase.auth().signOut()
+      .then(() => {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'LogIn' }],
+        });
+      })
+      .catch((error) => {
+        Alert.alert(error.message);
+      });
+  }
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => { handlePress(); }}
+    >
       <Text style={styles.label}>LogOut</Text>
     </TouchableOpacity>
   );
