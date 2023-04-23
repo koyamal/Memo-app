3924719142de1ab86dc53a68a823bb4d77506b0c
koyamal <tanakamaruko1121n@gmail.com>
2023-04-24 07:57:22 +0900

    Save sample data to Cloud firestore in MemoCreateScreen

--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -1,20 +1,39 @@
+/* eslint import/no-extraneous-dependencies: 0 */
 import React from 'react';
 import {
   View,
   StyleSheet,
   TextInput,
 } from 'react-native';
+import firebase from 'firebase';
 import CircleButton from '../components/CircleButton';
 import KeyboardSafeView from '../components/KeyboardSafeView';
 
 export default function MemoCreateScreen(props) {
   const { navigation } = props;
+
+  function handlePress() {
+    const db = firebase.firestore();
+    const ref = db.collection('memos');
+    ref.add({
+      bodyText: 'Hello',
+    })
+      .then((docRef) => {
+        /* eslint-disable-next-line */
+        console.log('Created. ', docRef.id);
+        navigation.goBack();
+      })
+      .catch((error) => {
+        /* eslint-disable-next-line */
+        console.log('Error. ', error);
+      });
+  }
   return (
     <KeyboardSafeView style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput value="" multiline style={styles.input} />
       </View>
-      <CircleButton name="check" onPress={() => { navigation.goBack(); }} />
+      <CircleButton name="check" onPress={() => { handlePress(); }} />
     </KeyboardSafeView>
   );
 }
