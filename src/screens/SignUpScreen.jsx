c1ce8163d35f812c6b101f691c3946ea43d99ecb
koyamal <tanakamaruko1121n@gmail.com>
2023-04-23 10:29:25 +0900

    Create handlePress in SignUpScreen

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -12,6 +12,14 @@ export default function SignUpScreen(props) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { navigation } = props;
+
+  function handlePress() {
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'MemoList' }],
+    });
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.inner}>
@@ -36,12 +44,7 @@ export default function SignUpScreen(props) {
         />
         <Button
           label="Submit"
-          onPress={() => {
-            navigation.reset({
-              index: 0,
-              routes: [{ name: 'MemoList' }],
-            });
-          }}
+          onPress={() => { handlePress(); }}
         />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Already registered?</Text>
