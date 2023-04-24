197efe490da73bbc5047be4ec163ebdba3a2614c
koyamal <tanakamaruko1121n@gmail.com>
2023-04-25 08:35:09 +0900

    Optimize rendering and Stop process when screen turns off

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -18,13 +18,14 @@ export default function MemoListScreen(props) {
     const db = firebase.firestore();
     const { currentUser } = firebase.auth();
     const ref = db.collection(`users/${currentUser.uid}/memos`);
-    ref.onSnapshot((snapshot) => {
+    const unsubscribe = ref.onSnapshot((snapshot) => {
       snapshot.forEach((doc) => {
         /* eslint-disable-next-line */
         console.log(doc.id, doc.data());
       });
     });
-  });
+    return unsubscribe;
+  }, []);
 
   return (
     <View style={styles.container}>
