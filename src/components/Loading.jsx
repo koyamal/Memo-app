313489c324fb67b5bf6121a7c08a76c0887d1e1c
koyamal <tanakamaruko1121n@gmail.com>
2023-04-26 17:05:57 +0900

    Add style to Loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -7,10 +7,29 @@ import {
 
 export default function Loading() {
   return (
-    <View>
-      <View>
+    <View style={styles.container}>
+      <View style={styles.inner}>
         <ActivityIndicator size="large" color="#000000" />
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    zIndex: 5,
+  },
+  inner: {
+    marginBottom: 80,
+  },
+});
