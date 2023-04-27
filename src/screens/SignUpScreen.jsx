68e74e33b34dd74372ffcc198cc1f9695457f876
koyamal <tanakamaruko1121n@gmail.com>
2023-04-28 07:45:39 +0900

    Change Error Message to translateErrors in SignUpScreen

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -9,6 +9,7 @@ import {
 } from 'react-native';
 import firebase from 'firebase';
 import Button from '../components/Button';
+import { translateErrors } from '../utils';
 
 export default function SignUpScreen(props) {
   const [email, setEmail] = useState('');
@@ -26,7 +27,8 @@ export default function SignUpScreen(props) {
           routes: [{ name: 'MemoList' }],
         });
       }).catch((error) => {
-        Alert.alert(error.code);
+        const errorMsg = translateErrors(error.code);
+        Alert.alert(errorMsg.title, errorMsg.description);
         /* eslint-disable-next-line */
         console.log(error.code, error.message);
       });
