b6cb44c9c5a1029296560fa84738831e9ba29ada
koyamal <tanakamaruko1121n@gmail.com>
2023-04-28 07:35:07 +0900

    Create Error Message

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,3 +6,22 @@ export function dateToString(date) {
   }
   return format(date, 'yyyy/M/d HH:mm');
 }
+
+export function translateErrors(code) {
+  const error = { title: 'Error:', description: 'Please Try Again.' };
+  switch (code) {
+    case 'auth/invalid-email':
+      error.description = 'Invalid Email Address';
+      break;
+    case 'auth/user-disabled':
+      error.description = 'Account is Disabled';
+      break;
+    case 'auth/user-not-found':
+      error.description = 'User Not Found';
+      break;
+    case 'auth/wrong-password':
+      error.description = 'Password is Incorrect';
+      break;
+    default:
+  }
+}
