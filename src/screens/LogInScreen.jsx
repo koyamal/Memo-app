f46912be60e22f297d7eb73fa157a1bfd8a44aa8
koyamal <tanakamaruko1121n@gmail.com>
2023-05-01 09:55:46 +0900

    Delete console.log in LogInScreen

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -36,10 +36,7 @@ export default function LogInScreen(props) {
   function handlePress() {
     setIsLoading(true);
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        const { user } = userCredential;
-        /* eslint-disable-next-line */
-        console.log(user.uid);
+      .then(() => {
         navigation.reset({
           index: 0,
           routes: [{ name: 'MemoList' }],
